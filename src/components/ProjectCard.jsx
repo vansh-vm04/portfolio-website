bed5de2c9a723eb25e71a064b63d88c14fc32bd7
Vansh <vanshmorwani1@gmail.com>
2025-05-02 00:22:35 +0530

    text-fix

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -38,7 +38,7 @@ const ProjectCard = ({ image, liveLink, gitLink, name, description, tools }) => 
         </a>
         <a href={liveLink} target="_blank" rel="noreferrer">
           <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none hover:cursor-pointer">
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-white bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
               View Live
             </span>
           </button>
