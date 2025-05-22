2ac23ef665dc42e5df7c78a4facf6cd93dd0a8e3
Vansh <vanshmorwani1@gmail.com>
2025-05-22 20:19:34 +0530

    nav edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-gray-900 fixed w-full z-20 top-0 start-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
         <a href="/#about" className="flex items-center space-x-3 rtl:space-x-reverse">
