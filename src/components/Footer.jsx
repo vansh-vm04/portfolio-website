8f7ac9b3098ad9c15236d516f6c22e4894fc1d70
Vansh <vanshmorwani1@gmail.com>
2025-06-30 22:53:04 +0530

    text fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { github } from "../utils/data";
 const Footer = () => {
   return (
     <div>
-      <footer className= "shadow-sm py-4 bg-gray-900">
+      <footer className= "shadow-sm pb-6 bg-gray-900">
         <div className="w-full max-w-screen-xl mx-auto ">
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
             Built and Designed By {" "}<a className="text-purple-500" href={github}>Vansh</a>
           </span>
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
             © 2025{" "} All Rights Reserved.
           </span>
         </div>
