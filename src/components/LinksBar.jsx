156e62f1be08d9be8b2da3b6d19755b9245d5347
Vansh <vanshmorwani1@gmail.com>
2025-07-23 21:54:59 +0530

    links change

--- src/components/LinksBar.jsx
+++ src/components/LinksBar.jsx
@@ -13,12 +13,6 @@ const SideComponent = () => {
       name: "LinkedIn",
     },
     {
-      id: 1,
-      icon: <FaSquareXTwitter size={24} />,
-      url: "https://x.com/vanshhCodes",
-      name: "X",
-    },
-    {
       id: 2,
       icon: <SiLeetcode size={24} />,
       url: "https://leetcode.com/vansh2704",
@@ -35,7 +29,7 @@ const SideComponent = () => {
           duration: 0.4,
           ease: "easeIn",
         }}
-        viewport={{ once: true }} className="max-md:hidden fixed top-1/3 left-3 z-50 flex flex-col gap-4">
+        viewport={{ once: true }} className="max-md:hidden fixed top-2/5 left-3 z-50 flex flex-col gap-4">
       {links.map((link) => (
         <a
           key={link.id}
